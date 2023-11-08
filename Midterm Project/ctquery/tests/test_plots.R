dc72cea260d3de48c44f30d486fa82ab396a90b1
juliastiller <148993755+juliastiller@users.noreply.github.com>
2023-11-08 16:27:20 -0500

    Julia working branch (#5)
    * Add files via upload
    
    * finalized features and added some test code
    
    - finalized features in app.r and ct-util.R: filter on status, histogram of intervention
    - added code to test.R that can be used to test ct-util.R
    
    * accidentally updated main instead of my branch (#4)
    
    * Julia working branch (#3)
    
    * Add files via upload
    
    * finalized features and added some test code
    
    - finalized features in app.r and ct-util.R: filter on status, histogram of intervention
    - added code to test.R that can be used to test ct-util.R
    
    * added test code for each of the 5 plots
    
    - still need to add more expectations
    
    * Add files via upload
    
    * Finally fixed the conditions histogram
    
    I think I removed all the old code I was playing around with but feel free to remove if you find any relics

--- Midterm Project/ctquery/tests/test_plots.R
+++ Midterm Project/ctquery/tests/test_plots.R
@@ -1,22 +1,12 @@
 library(testthat)
 source("ct-util.R")
 
-# Create a sample dataset for testing
-sample_data <- data.frame(
-  nct_id = 1:5,
-  phase = c("Phase 1", "Phase 2", "Phase 2", "Phase 3", "Phase 1"),
-  start_date = c("2020-09-08", "2008-08-31", "2017-05-01", "2007-09-30", "2006-07-31"),
-  completion_date = c("2025-12-31", "2009-09-30", "2024-03-30", "2010-12-31", "2015-12-31"),
-  condition_name = c("Non Small Cell Lung Carcinoma", "Depression", "Obesity", "Non Small Cell Lung Carcinoma", "Pain"),
-  country_name = c("United States", "Canada", "United States", "Puerto Rico", "United Kingdom"),
-  intervention_type = c("Drug", "Drug", "Biological", "Procedure", "Drug"),
-  brief_title = c("Trial 1", "Trial 2", "Trial 3", "Trial 4", "Trial 5"),
-  source_class = c("FED", "INDIV", "INDUSTRY", "NETWORK", "NIH"),
-  status = c("Completed", "Unknown", "Recruiting", "Approved for marketing", "Completed"),
-  is_fda_regulated_drug = c(TRUE, FALSE, TRUE, FALSE, TRUE)
-)
-
 test_that("Phase plot is generated correctly", {
+  # Create a sample dataset for testing
+  sample_data <- data.frame(
+    nct_id = 1:5,
+    phase = c("Phase 1", "Phase 2", "Phase 2", "Phase 3", "Phase 1")
+  )
   # Generate the phase plot
   plot <- plot_phase_histogram(sample_data)
 
@@ -26,6 +16,12 @@ test_that("Phase plot is generated correctly", {
 })
 
 test_that("Concurrent studies plot is generated correctly", {
+  # Create a sample dataset for testing
+  sample_data <- data.frame(
+    nct_id = 1:5,
+    start_date = c("2020-09-08", "2008-08-31", "2017-05-01", "2007-09-30", "2006-07-31"),
+    completion_date = c("2025-12-31", "2009-09-30", "2024-03-30", "2010-12-31", "2015-12-31")
+  )
   # Generate the concurrent studies plot
   plot <- plot_concurrent_studies(sample_data)
   
@@ -35,6 +31,11 @@ test_that("Concurrent studies plot is generated correctly", {
 })
 
 test_that("Conditions plot is generated correctly", {
+  # Create a sample dataset for testing
+  sample_data <- data.frame(
+    nct_id = 1:5,
+    condition_name = c("Non Small Cell Lung Carcinoma", "Non Small Cell Lung Carcinoma", "Non Small Cell Lung Carcinoma", "Non Small Cell Lung Carcinoma", "Pain")
+  )
   # Generate the conditions plot
   plot <- plot_conditions_histogram(sample_data)
   
@@ -44,6 +45,11 @@ test_that("Conditions plot is generated correctly", {
 })
 
 test_that("Countries plot is generated correctly", {
+  # Create a sample dataset for testing
+  sample_data <- data.frame(
+    nct_id = 1:5,
+    country_name = c("United States", "Canada", "United States", "Puerto Rico", "United Kingdom")
+  )
   # Generate the conditions plot
   plot <- plot_countries_frequency(sample_data)
   
@@ -53,6 +59,11 @@ test_that("Countries plot is generated correctly", {
 })
 
 test_that("Interventions plot is generated correctly", {
+  # Create a sample dataset for testing
+  sample_data <- data.frame(
+    nct_id = 1:5,
+    intervention_type = c("Drug", "Drug", "Biological", "Procedure", "Drug")
+  )
   # Generate the conditions plot
   plot <- plot_interventions_histogram(sample_data)
   
