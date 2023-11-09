af13507ace761fd63db1268248ecc3160884ff8e
juliastiller <148993755+juliastiller@users.noreply.github.com>
2023-11-08 22:15:11 -0500

    Add files via upload

--- Midterm Project/ctquery/test.R
+++ Midterm Project/ctquery/test.R
@@ -29,12 +29,14 @@ conditions_counts <- head(conditions$name)
 # test keywords: non-small
 # test sponsor: nih
 # test status: completed
-# test fda regulated: true
+# test fda drug: true
+# test fda device: false
 
 brief_title_kw = "non-small"
 source = "NIH"
 status = "Completed"
-fda = TRUE
+fda_drug = TRUE
+fda_device = FALSE
 
 # 1. Filter data by Brief Title Keywords
 if (brief_title_kw != "") {
@@ -60,13 +62,23 @@ if (!is.null(status)) {
 }
 
 # 4. Filter data by FDA Regulated Drug
-if ("TRUE" %in% fda) {
+if ("TRUE" %in% fda_drug) {
   ret <- ret %>%
     filter(is_fda_regulated_drug == TRUE)
 }
-if ("FALSE" %in% fda) {
+if ("FALSE" %in% fda_drug) {
   ret <- ret %>%
     filter(is_fda_regulated_drug == FALSE)
+}
+
+# 5. Filter data by FDA Regulated Device
+if ("TRUE" %in% fda_device) {
+  ret <- ret %>%
+    filter(is_fda_regulated_device == TRUE)
+}
+if ("FALSE" %in% fda_device) {
+  ret <- ret %>%
+    filter(is_fda_regulated_device == FALSE)
 }
 
 # LEFT JOIN conditions data into the studies data based on nct_id
@@ -164,21 +176,24 @@ ret = ret |>
      theme(axis.text.x = element_text(angle = 45, hjust = 1))  # Rotate x-axis labels for better readability
 
 # 3. Conditions histogram
+num_top_conditions = 5
 ret |>
-  plot_conditions_histogram()
+  plot_conditions_histogram(num_top_conditions)
 
 # plug into actual code
-# Find the top 5 most common conditions
+# Find the top n most common conditions
 x_grouped <- ret |>
   group_by(condition_name) |>
   summarize(n=n()) |>
   arrange(desc(n))
 
 top_conditions <- x_grouped$condition_name |>
-  head(5)
+  head(num_top_conditions)
 
-# Create a new column that determines whether the study is in one of those top 5 conditions or not
-x_grouped$condition_group <- ifelse(x_grouped$condition_name %in% top_conditions, x_grouped$condition_name, "Other")
+# Create a new column that determines whether the study is in one of those top n conditions or not
+x_grouped$condition_group <- ifelse(x_grouped$condition_name %in% top_conditions, 
+                                    x_grouped$condition_name, 
+                                    "Other")
 
 # Define a fixed set of conditions
 fixed_conditions <- append(top_conditions, "Other")
@@ -187,7 +202,6 @@ fixed_conditions <- append(top_conditions, "Other")
 condition_counts <- x_grouped |>
   group_by(condition_group)  |>
   summarize(total= sum(n))
-  #table(factor(ret$condition_group, levels = fixed_conditions))
 
 # Create a data frame with the fixed conditions and their counts
 condition_data <- data.frame(Condition = condition_counts$condition_group, Count = as.numeric(condition_counts$total))
@@ -208,15 +222,48 @@ ggplot(condition_data, aes(x = Condition, y = Count)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) # Rotate x-axis labels for better readability
 
 # 4. Countries histogram
+num_top_countries = 5
 ret |>
-  plot_countries_frequency()
+  plot_countries_frequency(num_top_countries)
+
+# Find the top n most common countries
 # plug into actual code
-ggplot(ret, aes(x = country_name)) +
-  geom_bar(fill = "skyblue", color = "black") +
+country_grouped <- ret |>
+  group_by(country_name) |>
+  summarize(n = n()) |>
+  arrange(desc(n))
+
+top_countries <- country_grouped$country_name |>
+  head(num_top_countries)
+
+# Create a new column that determines whether the study is in one of those top n countries or not
+country_grouped$country_group <- ifelse(country_grouped$country_name %in% top_countries, 
+                                        country_grouped$country_name, 
+                                        "Other")
+
+# Define a fixed set of countries
+fixed_countries <- append(top_countries, "Other")
+
+# Count country frequencies
+country_counts <- country_grouped |>
+  group_by(country_group)  |>
+  summarize(total = sum(n))
+
+# Create a data frame with the fixed countries and their counts
+country_data <- data.frame(Country = country_counts$country_group, Count = as.numeric(country_counts$total))
+
+# Order countries and create labels
+country_data$Country <- factor(country_data$Country, levels = fixed_countries)
+
+# Plot
+ggplot(country_data, aes(x = Country, y = Count)) +
+  geom_col(fill = "skyblue", color = "black") +
   xlab("Country") +
-  ylab("Frequency") +
+  ylab("Count") +
   labs(title = "Clinical Trial Country Distribution",  # Add title
        caption = "Source: https://clinicaltrials.gov/") +  # Add caption
+  scale_x_discrete(labels = scales::wrap_format(width = 15)) +  # Wrap x-axis labels for better presentation
+  # scale_y_log10() + # Scale y to better see smaller buckets
   theme_minimal() + # Use a minimal theme
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) # Rotate x-axis labels for better readability
 
@@ -227,8 +274,8 @@ ret |>
 # plug into actual code
 ggplot(ret, aes(x = intervention_type)) +
   geom_bar(fill = "skyblue", color = "black") +
-  xlab("Country") +
-  ylab("Frequency") +
+  xlab("Intervention") +
+  ylab("Count") +
   labs(title = "Clinical Trial Intervention Distribution",  # Add title
        caption = "Source: https://clinicaltrials.gov/") +  # Add caption
   theme_minimal() + # Use a minimal theme
